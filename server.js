8e9da4bb1056938d2e5494cc755e833406434685
Hanzou Hattori <hanzou.fuuma@gmail.com>
2018-01-28 10:09:40 +0000

    Functional prototype

--- server.js
+++ server.js
@@ -5,21 +5,32 @@ var app = express();
 
 app.use('/send', bodyParser.json());
 app.post('/send', function(req, res){
-    console.log('GET /')
+    console.log(Date.now() + ': POST on /send from ' + req.connection.remoteAddress);
     var process = require('child_process');
     var score = req.body.score;
     var address = req.body.address;
-    console.log('score = ' + score + ', address = ' + address);
-    process.exec('../Flaps/src/flappycoind getmininginfo', function (err,stdout,stderr) {
+    var payout = 10 + 2 * (score - 10);
+    if (payout > 250) {
+        console.log('possibly a cheater');
+        payout = 250;
+    }
+    console.log('score = ' + score + ', address = ' + address + ', calculated payout = ' + payout);
+    process.exec('../Flaps/src/flappycoind sendtoaddress ' + address + ' ' +
+        payout + ' "flappycoin-game score ' + score + '"', function(err,stdout,stderr) {
         if (err) {
             res.writeHead(403, {'Content-Type': 'text/plain'});
-            console.log(typeof(stderr));
-            console.log("\n"+stderr);
-            res.send(stderr);
+            console.log(typeof(stderr) + ': ' + stderr.trim());
+            res.end(stderr);
         } else {
-            console.log(stdout);
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end(stdout);
+            console.log(stdout.trim());
+            if (stdout.length == 65 && stdout[64] == '\n') {
+                // valid txid
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end(stdout.trim());
+            } else {
+                res.writeHead(403, {'Content-Type': 'text/plain'});
+                res.end(stdout);
+            }
         }
     });
 });
